1126291e8cf9d35af7f05e3221137adc371d3c59
GunnHentze <32563477+GunnHentze@users.noreply.github.com>
2017-10-06 11:21:17 +0200

    Opdateret roll()
    Har lavet roll() om, så alle seks værdier af terningerne forekommer lige hyppigt.

--- src/Dice.java
+++ src/Dice.java
@@ -1,12 +1,16 @@
 // Hej
 
 public class Dice {
+	private final int MAX = 6;
+	
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
+		/*float d1=(float)Math.random();   // 0-1
 		float d2=d1*5;                     // 0-5
 		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		return d3 + 1;                     // 1-6
+		*/
+		return (int)(Math.random()*MAX)+1; // 1-6
 	}
 	
 	// roll the die n times and print the values
